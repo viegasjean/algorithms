9e7267ac913b38a73cd5b156be13ebacb8daa6df
Jean Viegas <jeanmelg@gmail.com>
2022-10-10 15:09:39 -0300

    feat: :sparkles: study_schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
+    count = 0
+    for studant in permanence_period:
+        try:
+            entrada = int(studant[0])
+            saida = int(studant[1])
+            if entrada <= int(target_time) <= saida:
+                count += 1
+        except (TypeError, ValueError):
+            return None
+    return count
+
