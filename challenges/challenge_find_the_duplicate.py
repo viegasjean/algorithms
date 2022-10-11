26aebe854df9040637134b7240d84e3c388bfdf0
Jean Viegas <jeanmelg@gmail.com>
2022-10-10 21:34:04 -0300

    feat: :sparkles: find the duplicate function

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,10 @@
 def find_duplicate(nums):
     """Faça o código aqui."""
+    if type(nums) != list or len(nums) <= 1 or nums == str:
+        return False
+    for num in nums:
+        if type(num) == str or num < 0:
+            return False
+        if nums.count(num) > 1:
+            return num
+    return False
