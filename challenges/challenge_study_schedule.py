d17c322a022c36c2da532a6dd7bd8418bbe26938
Jean Viegas <jeanmelg@gmail.com>
2022-10-12 00:32:27 -0300

    chore: clean code

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,13 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
     count = 0
     if type(target_time) != int:
         return None
     for studant in permanence_period:
-        entrada = studant[0]
-        saida = studant[1]
+        entrada, saida = studant
         if type(entrada) != int or type(saida) != int:
             return None
-        if entrada <= int(target_time) <= saida:
+        if entrada <= target_time <= saida:
             count += 1
     return count
