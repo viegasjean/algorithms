bc031f09ca8fdcd95bd9854f387b8572c093888b
Jean Viegas <jeanmelg@gmail.com>
2022-10-12 00:01:20 -0300

    refactor: :recycle: change strategy from try except to if conditions

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
     count = 0
+    if type(target_time) != int:
+        return None
     for studant in permanence_period:
-        try:
-            entrada = int(studant[0])
-            saida = int(studant[1])
-            if entrada <= int(target_time) <= saida:
-                count += 1
-        except (TypeError, ValueError):
+        entrada = studant[0]
+        saida = studant[1]
+        if type(entrada) != int or type(saida) != int:
             return None
+        if entrada <= int(target_time) <= saida:
+            count += 1
     return count
