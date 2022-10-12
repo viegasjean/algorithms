a451c09d69ec241f1990f132cbc1c593097a6a73
Jean Viegas <jeanmelg@gmail.com>
2022-10-11 22:12:10 -0300

    refactor: :recycle: change strategy from binary search to simple for loop

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,33 +1,30 @@
-def binary_search(numbers, target):
-    numbers.sort()
-    start = 0
-    end = len(numbers) - 1
+# def binary_search(numbers, target):
+#     numbers.sort()
+#     start = 0
+#     end = len(numbers) - 1
 
-    while start <= end:
-        mid = (start + end) // 2
+#     while start <= end:
+#         mid = (start + end) // 2
 
-        if numbers[mid] == target:
-            return target
+#         if numbers[mid] == target:
+#             return target
 
-        if target < numbers[mid]:
-            end = mid - 1
-        else:
-            start = mid + 1
+#         if target < numbers[mid]:
+#             end = mid - 1
+#         else:
+#             start = mid + 1
 
-    return False
+#     return False
 
 
 def find_duplicate(nums):
     if type(nums) != list or len(nums) <= 1:
         return False
-    # print('nums', nums)
-    filtered = []
-    for n in nums:
+    nums.sort()
+    for i, n in enumerate(nums[:-1]):
         if type(n) == str or n < 0:
             return False
-        if n not in filtered:
-            filtered.append(n)
-        else:
-            return binary_search(nums, n)
+        if n == nums[i + 1]:
+            return n
 
     return False
